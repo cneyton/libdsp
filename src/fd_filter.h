3b24d840d0b72b8ae18033770db3dcb855360cd9
cne <c.neyton@nateo-healthcare.com>
2020-02-17 09:58:22 +0100

    add fd filter
    Signed-off-by: cne <c.neyton@nateo-healthcare.com>

---
+++ src/fd_filter.h
@@ -0,0 +1,73 @@
+#ifndef FD_FILTER_H
+#define FD_FILTER_H
+
+#include "sigpack.h"
+#include "filter.h"
+#include "chunk.h"
+#include "link.h"
+
+namespace filter
+{
+
+template<typename T1 = arma::cx_double, typename T2 = double>
+class fd: public Filter
+{
+public:
+    fd(common::Logger logger, arma::uword nfft, arma::vec window):
+        Log(logger), Filter(logger), nfft_(nfft),fftw_(sp::FFTW(nfft, FFTW_MEASURE)), window_(window)
+    {
+        /* TODO: do this at link  <14-02-20, cneyton> */
+        // Create fftw plan by first application of fft
+        arma::Col<T1> vec(nfft);
+        fftw_.fft_cx(vec, vec);
+    }
+
+    virtual ~fd() {};
+
+    virtual int activate()
+    {
+        log_debug(logger_, "fd filter {} activated", this->name_);
+
+        auto in_chunk = std::make_shared<Chunk>(logger_);
+        if (!inputs_.at(0)->pop(in_chunk)) {
+            ready_ = false;
+            return 0;
+        }
+
+        auto size = arma::size(*in_chunk);
+        auto out_chunk = std::make_shared<Chunk>(logger_, arma::size(1, size.n_cols, size.n_slices));
+
+        for (uint k = 0; k < size.n_slices; k++) {
+            for (uint j = 0; j < size.n_cols; j++) {
+                arma::Col<T1> in = in_chunk->slice(k).col(j);
+                //in.print();
+                fftw_.fft_cx(in, in);
+                //in.print();
+                arma::Col<T2> psd = arma::square(arma::abs(in));
+                arma::Col<T2> w   = arma::regspace<arma::Col<T2>>(0, nfft_-1)
+                    - static_cast<T2>(nfft_)/2;
+                T2 m0 = arma::sum(psd);
+                T2 m1 = arma::dot(psd, w);
+                (*out_chunk)(0, j, k) = 1/static_cast<T2>(nfft_) * m1/m0;
+            }
+        }
+
+        if (verbose_) {
+            in_chunk->print();
+            out_chunk->print();
+        }
+
+        outputs_.at(0)->push(out_chunk);
+
+        return 0;
+    }
+
+private:
+    arma::uword nfft_;
+    sp::FFTW    fftw_;
+    arma::vec   window_;
+};
+
+} /* namespace filter */
+
+#endif /* FD_FILTER_H */
---
+++ test/fd_filter_test.cpp
@@ -0,0 +1,78 @@
+#include <thread>
+#include <chrono>
+#include <random>
+
+#include "common/log.h"
+#include "common/data.h"
+
+#include "fd_filter.h"
+#include "pipeline.h"
+#include "source_filter.h"
+#include "sink_filter.h"
+
+#include "spdlog/sinks/stdout_color_sinks.h"
+common::Logger logger(spdlog::stdout_color_mt("dsp"));
+
+uint16_t nb_samples = 36;
+uint16_t nb_slots   = 7;
+uint16_t nb_frames  = 128;
+size_t elt_size = nb_samples * nb_slots * 4;
+
+void producer_th_func(common::data::Producer& p)
+{
+    std::random_device rd;
+    std::uniform_int_distribution dist(0, 254);
+    while (1) {
+        common::data::ByteBuffer buf(elt_size, 1);
+        std::transform(buf.begin(), buf.end(), buf.begin(),
+                       [&](int x){return x * static_cast<uint8_t>(dist(rd));});
+        p.push(common::data::type::us, buf);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+}
+
+void pipeline_th_func(Pipeline& pipeline)
+{
+    int ret;
+    while (1) {
+        ret = pipeline.run();
+        common_die_zero_void(logger, ret, "pipeline run error");
+        if (ret == 0) {
+        }
+    }
+}
+
+int main()
+{
+    logger->set_level(spdlog::level::debug);
+    common::data::Handler data_handler(logger);
+    common::data::Producer producer(logger, &data_handler);
+
+    Pipeline pipeline(logger);
+
+    auto source_filter = new filter::Source(logger, &data_handler, common::data::type::us);
+    source_filter->set_format(sizeof(arma::cx_float), nb_samples, nb_slots);
+    source_filter->set_nb_frames(nb_frames);
+    pipeline.add_filter(source_filter);
+
+    auto sink_filter = new filter::Sink(logger);
+    pipeline.add_filter(sink_filter);
+
+    auto fd_filter = new filter::fd<arma::cx_double, double>(logger, nb_frames, arma::vec(nb_frames, arma::fill::ones));
+    //fd_filter->set_verbose();
+    pipeline.add_filter(fd_filter);
+
+    pipeline.link(source_filter, fd_filter);
+    pipeline.link(fd_filter, sink_filter);
+
+    data_handler.reinit_queue(common::data::type::us, elt_size, 1000);
+
+    std::thread pipeline_th(pipeline_th_func, std::ref(pipeline));
+    std::thread producer_th(producer_th_func, std::ref(producer));
+
+    pipeline_th.join();
+    producer_th.join();
+
+    return 0;
+}
+
