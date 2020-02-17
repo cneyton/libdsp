a9bc7ba43c47cf802b3739c11776f43831203bec
cne <c.neyton@nateo-healthcare.com>
2020-02-17 09:57:02 +0100

    clean fft filter
    Signed-off-by: cne <c.neyton@nateo-healthcare.com>

--- src/fft_filter.h
+++ src/fft_filter.h
@@ -25,19 +25,21 @@ public:
     virtual int activate()
     {
         log_debug(logger_, "fft filter {} activated", this->name_);
+
         auto in_chunk = std::make_shared<Chunk>(logger_);
         if (!inputs_.at(0)->pop(in_chunk)) {
             ready_ = false;
             return 0;
         }
+
         auto size = arma::size(*in_chunk);
         auto out_chunk = std::make_shared<Chunk>(*in_chunk);
 
         for (uint k = 0; k < size.n_slices; k++) {
             for (uint j = 0; j < size.n_cols; j++) {
                 auto ptr  = out_chunk->slice_colptr(k, j);
-                arma::Col<T> in(ptr, size.n_rows, false, true) ;
-                fftw_.fft_cx(in, in);
+                arma::Col<T> vec(ptr, size.n_rows, false, true) ;
+                fftw_.fft_cx(vec, vec);
             }
         }
 
