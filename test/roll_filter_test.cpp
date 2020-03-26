098d94a4de6bd96d5e077fac617a72919fbc6526
cne <c.neyton@nateo-healthcare.com>
2020-03-26 19:09:59 +0100

    [TEST] update roll_filter_test
    Signed-off-by: cne <c.neyton@nateo-healthcare.com>

--- test/roll_filter_test.cpp
+++ test/roll_filter_test.cpp
@@ -1,100 +1,71 @@
-#include <thread>
-#include <chrono>
-#include <random>
+#include "test_utils.h"
 
-#include "common/log.h"
-#include "common/data.h"
-
+#include "iir_filter.h"
 #include "roll_filter.h"
-#include "pipeline.h"
-#include "source_filter.h"
-#include "sink_filter.h"
+#include "fd_filter.h"
 
+#include "spdlog/common.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
-common::Logger logger(spdlog::stdout_color_mt("dsp"));
 
-// input params
-using iT = uint8_t;
-using oT = double;
+using iT = arma::cx_double;
+using oT = arma::cx_double;
 
-constexpr uint16_t nb_samples = 5;
-constexpr uint16_t nb_slots   = 1;
-constexpr uint16_t nb_frames  = 6;
-constexpr uint16_t nb_frames_out = nb_frames * 3;
-constexpr size_t   elt_size   = nb_samples * nb_slots * sizeof(iT);
-constexpr uint     nb_tot_frames = 60;
-constexpr uint     period = 1;
-
-// Filter params
-constexpr uint skip = 4;
-
-class Handler: public common::data::Handler
+int main(int argc, char * argv[])
 {
-public:
-    Handler(common::Logger logger, Pipeline * pipeline):
-        common::data::Handler(logger), pipeline_(pipeline) {}
-    virtual ~Handler() {}
+    if (argc != 4)
+        return -1;
 
-    virtual int data_pushed()
-    {
-        pipeline_->wakeup();
-        return 0;
-    }
+    std::string filename_in(argv[1]);
+    std::string filename_out(argv[2]);
+    std::string filename_params(argv[3]);
 
-private:
-    Pipeline * pipeline_;
-};
-
-void producer_th_func(common::data::Producer& p)
-{
-    for (uint i = 0; i < nb_tot_frames; i++) {
-        common::data::ByteBuffer buf(elt_size, i);
-        p.push(common::data::type::us, buf);
-        std::this_thread::sleep_for(std::chrono::milliseconds(period));
-    }
-}
-
-void pipeline_th_func(Pipeline& pipeline)
-{
-    pipeline.run();
-}
-
-int main()
-{
-    logger->set_level(spdlog::level::info);
+    common::Logger logger(spdlog::stdout_color_mt("dsp"));
 
     Pipeline pipeline(logger);
     Handler data_handler(logger, &pipeline);
-    common::data::Producer producer(logger, &data_handler);
+    Producer<iT> producer(logger, &data_handler, filename_in);
 
-    arma::SizeCube fmt_in = arma::SizeCube(nb_frames, nb_samples, nb_slots);
-    auto source_filter = new filter::source<iT, oT>(logger, &data_handler, common::data::type::us);
-    source_filter->set_chunk_size(nb_frames, nb_samples, nb_slots);
-    pipeline.add_filter(std::unique_ptr<Filter>(source_filter));
+    auto fmt_data = producer.get_fmt();
+    std::cout << "Input:\n"
+              << "  size: (" << fmt_data.n_rows << "," << fmt_data.n_cols << "," << fmt_data.n_slices << ")\n"
+              << "------------------------------\n";
 
-    arma::SizeCube fmt_out = arma::SizeCube(nb_frames_out, nb_samples, nb_slots);
-    auto sink_filter = new filter::sink<oT>(logger);
-    sink_filter->set_verbose();
-    pipeline.add_filter(std::unique_ptr<Filter>(sink_filter));
+    cnpy::NpyArray nskip_np  = cnpy::npz_load(filename_params, "nskip");
+    cnpy::NpyArray n_in_np   = cnpy::npz_load(filename_params, "n_in");
+    cnpy::NpyArray n_out_np  = cnpy::npz_load(filename_params, "n_out");
 
-    auto roll_filter = new filter::roll<oT>(logger, skip);
-    pipeline.add_filter(std::unique_ptr<Filter>(roll_filter));
+    arma::uword nskip(*nskip_np.data<arma::uword>());
+    arma::uword n_in(*n_in_np.data<arma::uword>());
+    arma::uword n_out(*n_out_np.data<arma::uword>());
 
     std::cout << "Filter params:\n"
-              << "  skip: " << skip << "\n"
+              << "   n_in:  " << n_in << "\n"
+              << "   n_out: " << n_in << "\n"
+              << "   nskip: " << nskip << "\n"
               << "------------------------------\n";
+
+    arma::SizeCube fmt_in(n_in, fmt_data.n_cols, fmt_data.n_slices);
+    auto source_filter = new filter::source<iT, oT>(logger, &data_handler, common::data::type::us);
+    source_filter->set_chunk_size(fmt_in.n_rows, fmt_in.n_cols, fmt_in.n_slices);
+    pipeline.add_filter(std::unique_ptr<Filter>(source_filter));
+
+    arma::SizeCube fmt_out(n_out, fmt_in.n_cols, fmt_in.n_slices);
+    auto roll_filter = new filter::roll<oT>(logger, nskip);
+    pipeline.add_filter(std::unique_ptr<Filter>(roll_filter));
+
+    auto sink_filter = new NpySink<oT>(logger, fmt_data);
+    pipeline.add_filter(std::unique_ptr<Filter>(sink_filter));
 
     pipeline.link<oT>(source_filter, roll_filter, fmt_in);
     pipeline.link<oT>(roll_filter, sink_filter, fmt_out);
 
-    data_handler.reinit_queue(common::data::type::us, elt_size, 1000);
-
-    std::thread pipeline_th(pipeline_th_func, std::ref(pipeline));
-    std::thread producer_th(producer_th_func, std::ref(producer));
+    std::thread pipeline_th(&Pipeline::run, &pipeline);
+    std::thread producer_th(&Producer<iT>::run, &producer);
 
     producer_th.join();
-    pipeline.stop();
     pipeline_th.join();
+
+    sink_filter->dump(filename_out);
 
     pipeline.print_stats();
 
